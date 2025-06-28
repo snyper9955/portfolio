3ee926c75369b6e5bc87231a7181617aef80f925
snyper9955 <162612396+snyper9955@users.noreply.github.com>
2025-06-28 18:22:21 +0530

    Force redeploy

--- src/page/VideoProj.jsx
+++ src/page/VideoProj.jsx
@@ -39,7 +39,7 @@ const WebProj = () => {
   controls
 >
   <source src="/V1.mp4" type="video/mp4" />
-  Your browser does not support the video tag.
+  Your browser does not support the video tag.hh
 </video>
 
 
